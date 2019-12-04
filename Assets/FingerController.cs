d0fdb0aa25bbe18e1b1bce2cd4b09df5f28fa6a8
Paul Leelaviwatana <apaulture@gmail.com>
2019-12-04 13:55:39 -0800

    - Added 3 other fingers - Reshaped model to look like a hand - Finger now has 3 parts (top, mid, bottom), top and mid are dynamic while bottom is static - Top finger bends twice as much as middle finger, see bendAngle *= 0.5f in script - FingerController.cs -- Added nonfunctioning multithreading -- Takes data from Arduino and splits it using , as delimiter and rotates the fingers based on each bendAngle element in array

--- Assets/FingerController.cs
+++ Assets/FingerController.cs
@@ -1,27 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using System.Threading;
+
+// This script is attached to each of the five fingers (top and bottom portions)
 
 public class FingerController : MonoBehaviour
 {
-    SerialPort serial;
     string portName;
+    int baudRate;
+    string arduinoMessage;
+    string[] splitMessage;
+    float[] bendAngle;
 
-    // Start is called before the first frame update
+    SerialPort serial;
+    Thread thread;
+    Queue outputQueue;
+    Queue inputQueue;
+
     void Start()
     {
-        portName = "COM6"; // Enter the port name here from Tools > Port
-        serial = new SerialPort(portName, 9600);
-        serial.Open();
+        portName = "/dev/cu.usbmodem146101"; // Enter the port name here from Tools > Port
+        baudRate = 9600;
+
+        serial = new SerialPort(portName, baudRate);
+        serial.ReadTimeout = 50;
+        serial.Open(); // Open port for serial communication
+
+        // StartThread();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        print(serial.ReadLine());
-        float bendAngle = float.Parse(serial.ReadLine());
-        print(bendAngle);
-        transform.eulerAngles = new Vector3(-90f, -bendAngle, 0);
+        arduinoMessage = serial.ReadLine();
+        splitMessage = arduinoMessage.Split(','); // split message into multiple bend angles
+
+        for (int i = 1; i <= 5; i++)
+        {
+            bendAngle[i] = float.Parse(splitMessage[i]);
+
+            // Distal phalanx (top joint) bends 2x as much as middle joint
+            if (gameObject.CompareTag("top") && transform.name == "finger" + i)
+            {
+                transform.eulerAngles = new Vector3(-bendAngle[i], 0, 0);
+            }
+
+            // Middle phalanx (middle joint) bends 1/2 the angle of top joint
+            if (gameObject.CompareTag("mid") && transform.name == "finger" + i)
+            {
+                bendAngle[i] *= 0.5f; // reduce bend angle by a half since the mid portion bends twice less than top
+                transform.eulerAngles = new Vector3(-bendAngle[i], 0, 0);
+            }
+        }
+    }
+
+    /*
+     * All code below is for multithreading but isn't currently working
+     */
+
+    void StartThread()
+    {
+        // Synchronized queues allow messages to be sent and received across threads
+        // When this script wants to talk to Arduino, it places the message in an input queue
+        outputQueue = Queue.Synchronized(new Queue());
+        inputQueue = Queue.Synchronized(new Queue());
+
+        thread = new Thread(ThreadLoop);
+        thread.Start();
+    }
+
+    void ThreadLoop()
+    {
+        
+
+        while (true)
+        {
+            if (outputQueue.Count != 0)
+            {
+                string command = (string)outputQueue.Dequeue();
+                WriteToArduino(command);
+            }
+        }
+    }
+
+    public void SendToArduino(string command)
+    {
+        outputQueue.Enqueue(command); // adds "command" to end of queue to be sent out to Arduino
+    }
+
+    public string ReadFromArduino()
+    {
+        if (inputQueue.Count == 0) // if no input, don't do anything
+            return null;
+
+        return (string) inputQueue.Dequeue(); // removes and returns object at beginning of queue
+    }
+
+    public void WriteToArduino(string message)
+    {
+        serial.WriteLine(message);
+        serial.BaseStream.Flush();
     }
 }
--- FlexController/FlexController.ino
+++ FlexController/FlexController.ino
@@ -19,12 +19,12 @@ const int FLEX_PIN = A0; // Pin connected to voltage divider output
 // Measure the voltage at 5V and the actual resistance of your
 // 47k resistor, and enter them below:
 const float VCC = 4.98; // Measured voltage of Ardunio 5V line
-const float R_DIV = 47500.0; // Measured resistance of 3.3k resistor
+const float R_DIV = 10000.0; // Measured resistance of 3.3k resistor
 
 // Upload the code, then try to adjust these values to more
 // accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 23540.0; // resistance when straight
-const float BEND_RESISTANCE = 38500.0; // resistance at 90 deg
+const float STRAIGHT_RESISTANCE = 4090.0; // resistance when straight
+const float BEND_RESISTANCE = 2500.0; // resistance at 90 deg
 
 void setup() 
 {
@@ -38,7 +38,7 @@ void loop()
   int flexADC = analogRead(FLEX_PIN);
   float flexV = flexADC * VCC / 1023.0;
   float flexR = flexV/(VCC-flexV)*10000;
-  //Serial.println("Resistance: " + String(flexR/1000) + " kilo-ohms");
+  // Serial.println("Resistance: " + String(flexR/1000) + " kilo-ohms");
 
   // Use the calculated resistance to estimate the sensor's
   // bend angle:
